faacb3801b72a3014e169e1c139a40fc4cdf4fbe
Davi F <davi.felix.lima@gmail.com>
2021-10-19 20:41:30 -0300

    message list component

--- frontend/src/components/MessageList/index.tsx
+++ frontend/src/components/MessageList/index.tsx
@@ -1,39 +1,49 @@
+import { useEffect, useState } from 'react';
+
 import styles from './styles.module.scss';
+
+import { api } from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
+interface User {
+  name: string;
+  avatar_url: string;
+}
+
+interface Message {
+  id: string;
+  text: string;
+  user: User;
+}
+
 export function MessageList() {
+
+  const [messages, setMessage] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last-three/').then(res => {
+      setMessage(res.data);
+    })
+  }, []);
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logo} alt="DoWhile 2021" />
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, vai ser o melhor de todos. Vamo pra cimaa!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/davifelix5.png" alt="Davi Félix" />
-            </div>
-            <span>Davi Félix</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, vai ser o melhor de todos. Vamo pra cimaa!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/davifelix5.png" alt="Davi Félix" />
-            </div>
-            <span>Davi Félix</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, vai ser o melhor de todos. Vamo pra cimaa!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/davifelix5.png" alt="Davi Félix" />
-            </div>
-            <span>Davi Félix</span>
-          </div>
-        </li>
+        {messages.map(message => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
