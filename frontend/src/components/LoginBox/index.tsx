1aeee4871fffb560c155afdf748ddf3d2f38d636
Davi F <davi.felix.lima@gmail.com>
2021-10-19 21:16:20 -0300

    created auth-context

--- frontend/src/components/LoginBox/index.tsx
+++ frontend/src/components/LoginBox/index.tsx
@@ -1,11 +1,17 @@
+import { useContext } from 'react';
+
 import styles from './styles.module.scss';
 import { VscGithubInverted } from 'react-icons/vsc';
+import { AuthContext } from '../../contexts/authContext';
 
 export function LoginBox() {
+  
+  const { signInUrl, user } = useContext(AuthContext);
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGitHub}>
+      <a href={signInUrl} className={styles.signInWithGitHub}>
         <VscGithubInverted size={24} />
         Entrar com GitHub
       </a>
