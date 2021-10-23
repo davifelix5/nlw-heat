692a8d194344e4c496d952b765e075331ecf1199
Davi F <davi.felix.lima@gmail.com>
2021-10-23 14:30:19 -0300

    added loader when sending message

--- frontend/src/components/SendMessageForm/index.tsx
+++ frontend/src/components/SendMessageForm/index.tsx
@@ -9,6 +9,7 @@ import { VscSignOut, VscGithubInverted } from 'react-icons/vsc';
 import { api } from '../../services/api';
 
 import { FeedbackMessage } from '../FeedbackMessage';
+import { Loader } from '../Loader';
 
 const TWO_SECONDS = 2 * 1000;
 
@@ -18,27 +19,34 @@ export function SendMessageForm() {
   
   const [message, setMessage] = useState('');
   const [feedbackMessage, setFeedbackMessage] = useState('');
+  const [sending, setSending] = useState(false);
   const [success, setSuccess] = useState<null | boolean>(null);
   
   async function handleFormSubmit(e: React.FormEvent) {
+    let timeout: number | undefined;
     e.preventDefault();
     if (!message.trim()) {
       return;
     }
-    try {
-      await api.post('messagess/', { message });
-      setFeedbackMessage('Mensagem enviada com sucesso!');
-      setSuccess(true);
-      setMessage('');
-    } catch (err) {
-      setFeedbackMessage('Erro ao enviar a mensagem');
-      setSuccess(false);
-    } finally {
-      setTimeout(() => {
-        setSuccess(null)
-        setFeedbackMessage('');
-      }, TWO_SECONDS);
-    }
+    setSending(true);
+    api.post('messages/', { message })
+      .then(() => {
+        setFeedbackMessage('Mensagem enviada com sucesso!');
+        setSuccess(true);
+        setMessage('');
+      })
+      .catch(() => {
+        setFeedbackMessage('Erro ao enviar a mensagem');
+        setSuccess(false);
+      })
+      .finally(() => {
+        setSending(false);
+        timeout = setTimeout(() => {
+          setSuccess(null);
+          setFeedbackMessage('');
+        }, TWO_SECONDS);
+      });
+      return () => clearTimeout(timeout);
   }
 
   return (
@@ -71,7 +79,11 @@ export function SendMessageForm() {
           id="message"
           placeholder="Qual sua expectativa para o evento?"
         />
-        <button type="submit">Enviar mensagem</button>
+        <button type="submit">
+          {sending ? (
+            <Loader size={30} primaryColor="#202024" secondaryColor="#fff"/>
+          ) : 'Enviar mensagem'}
+        </button>
       </form>
     </div>
     </>
